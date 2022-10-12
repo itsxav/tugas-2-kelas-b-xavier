5f237ecad8c71294600da530736e191b61a92d82
itsxav <xavierturambi2@gmail.com>
2022-10-12 23:10:18 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    // fontSize: {
+    //   xxs: '11px'
+    // }
+    
+    // extend: {
+    //   // backgroundImage: {
+    //   //   'header': "url('\src\assets\img\avengers-marvel1.svg')"
+    //   // }
+    // },
+  },
+  plugins: [],
+}
